11ab15ffb6231bfac6ed725e54be3faaf7433ce6
coderaulia <auliasw89@gmail.com>
2021-09-28 01:04:51 +0700

    router setting, db seed and add controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controller\AdminController;
+use App\Http\Controller\ContactController;
+use App\Http\Controller\HomeController;
+use App\Http\Controller\PortfolioController;
+use App\Http\Controller\PostController;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,5 +28,52 @@ Route::get('/about-us', function() {
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    $id = Auth::user()->id;
+    $user = User::find($id);
     return view('dashboard');
 })->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+    // BEM naming
+
+    // Contact page admin
+    Route::prefix('admin/contact')->group(function(){
+        Route::get('/all', [ContactController::class, 'contactView'])->name('contact.all');
+        Route::post('/save', [ContactController::class, 'contactSave'])->name('contact.save');
+        Route::get('/edit/{id}', [ContactController::class, 'contactEdit'])->name('contact.edit');
+        Route::post('/update', [ContactController::class, 'contactUpdate'])->name('contact.update');
+        Route::get('/delete/{id}', [ContactController::class, 'contactDelete'])->name('contact.delete');
+    });
+
+    // Message from Contact Page
+    Route::prefix('admin/message')->group(function(){
+        Route::get('/all', [ContactController::class, 'messageView'])->name('message.all');
+    });
+
+    // Portfolio admin
+    Route::prefix('admin/portfolio')->group(function(){
+        Route::get('/all', [PortfolioController::class, 'portfolioView'])->name('portfolio.all');
+        Route::post('/save', [PortfolioController::class, 'portfolioSave'])->name('portfolio.save');
+        Route::get('/edit/{id}', [PortfolioController::class, 'portfolioEdit'])->name('portfolio.edit');
+        Route::post('/update', [PortfolioController::class, 'portfolioUpdate'])->name('portfolio.update');
+        Route::get('/delete/{id}', [PortfolioController::class, 'portfolioDelete'])->name('portfolio.delete');
+    });
+    
+    // Post admin
+    Route::prefix('admin/post')->group(function(){
+        Route::get('/all', [PostController::class, 'postView'])->name('post.all');
+        Route::post('/save', [PostController::class, 'postSave'])->name('post.save');
+        Route::get('/edit/{id}', [PostController::class, 'postEdit'])->name('post.edit');
+        Route::post('/update', [PostController::class, 'postUpdate'])->name('post.update');
+        Route::get('/delete/{id}', [PostController::class, 'postDelete'])->name('post.delete');
+    }); 
+    
+    // Post admin
+    Route::prefix('admin/category')->group(function(){
+        Route::get('/all', [PostController::class, 'categoryView'])->name('category.all');
+        Route::post('/save', [PostController::class, 'categorySave'])->name('category.save');
+        Route::get('/edit/{id}', [PostController::class, 'categoryEdit'])->name('category.edit');
+        Route::post('/update', [PostController::class, 'categoryUpdate'])->name('category.update');
+        Route::get('/delete/{id}', [PostController::class, 'categoryDelete'])->name('category.delete');
+    });
+});
